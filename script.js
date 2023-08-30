79176bc4f330496da927422919a5d13772bb0015
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-08-30 14:25:52 -0500

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get password and confirmpassword into variables
+
+
+// Write function for checking if passwords are equal
+
+
+// If not equal turn border color to red and display error
+
+// If equal remove red border and error
